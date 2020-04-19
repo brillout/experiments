cc3cafc085cae9caedc6059954c2cdb205b1c7c7
Romuald Brillout <github.com@brillout.com>
2020-04-19 22:52:56 +0200

    improve MyForm example

--- MyForm.tsx
+++ MyForm.tsx
@@ -5,20 +5,23 @@ export { MyForm };
 
 @reactiveView
 class MyForm {
-  inputText = 'change me';
+  inputed_text = 'change me';
   view() {
     // This view is reactive: when the user changes
     // the `<input>` value, this view is re-rendered.
     return <>
-      <div>I'm automatically refreshed: {this.inputText}</div>
-      <input
-        type='text'
-        defaultValue={this.inputText}
-        onChange={ev => {
-          this.inputText = ev.target.value;
-        }}
-      />
+      <div>I'm automatically refreshed: {this.inputed_text}</div>
+      <TextInput text={this.inputed_text} onTextChange={newText => this.inputed_text = newText} />
     </>;
   }
 }
 
+function TextInput({text, onTextChange}) {
+  return (
+    <input
+      type='text'
+      defaultValue={text}
+      onChange={ev => onTextChange(ev.target.value)}
+    />
+  );
+}
